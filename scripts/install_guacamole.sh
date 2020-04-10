8eb3e3b2180699a2d8735a90669be97ff015c1ea
wojiushixiaobai <296015668@qq.com>
2020-04-10 10:19:08 +0800

    [Update]更新

--- scripts/install_guacamole.sh
+++ scripts/install_guacamole.sh
@@ -5,26 +5,33 @@ BASE_DIR=$(dirname "$0")
 PROJECT_DIR=$(dirname $(cd $(dirname "$0");pwd))
 source ${PROJECT_DIR}/config.conf
 
+function remove_guacamole() {
+    docker stop jms_guacamole
+    docker rm jms_guacamole
+}
+
 function start_guacamole() {
     echo ">> Install Jms_guacamole"
-    if [ ! "$(docker ps -a | grep jms_guacamole | grep $Version)" ]; then
-        if [ "$(docker ps -a | grep jms_guacamole)" ]; then
-            docker stop jms_guacamole
-            docker rm jms_guacamole
-        fi
-        docker run --name jms_guacamole -d -p 127.0.0.1:8081:8080 -e JUMPSERVER_SERVER=http://$Server_IP:8080 -e BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN --restart=always jumpserver/jms_guacamole:$Version
+    docker run --name jms_guacamole -d -p 127.0.0.1:8081:8080 -e JUMPSERVER_SERVER=http://$Server_IP:8080 -e BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN --restart=always jumpserver/jms_guacamole:$Version
+}
+
+function check_guacamole() {
+    if [ "$(docker exec jms_guacamole env | grep BOOTSTRAP_TOKEN | cut -d = -f2)" != "$BOOTSTRAP_TOKEN" ]; then
+        remove_guacamole
+        start_guacamole
+    fi
+    if [ ! "$(docker exec jms_guacamole env | grep $Server_IP )" ]; then
+        remove_guacamole
+        start_guacamole
     fi
 }
 
 function main() {
     if [ ! "$(docker ps | grep jms_guacamole:$Version)" ]; then
+        remove_guacamole
         start_guacamole
     else
-        if [ "$(docker exec jms_guacamole env | grep BOOTSTRAP_TOKEN | cut -d = -f2)" != "$BOOTSTRAP_TOKEN" ]; then
-            docker stop jms_guacamole
-            docker rm jms_guacamole
-            start_guacamole
-        fi
+        check_guacamole
     fi
 }
 
--- scripts/install_koko.sh
+++ scripts/install_koko.sh
@@ -5,26 +5,33 @@ BASE_DIR=$(dirname "$0")
 PROJECT_DIR=$(dirname $(cd $(dirname "$0");pwd))
 source ${PROJECT_DIR}/config.conf
 
+function remove_koko() {
+    docker stop jms_koko
+    docker rm jms_koko
+}
+
 function start_koko() {
     echo ">> Install Jms_koko"
-    if [ ! "$(docker ps -a | grep jms_koko:$Version)" ]; then
-        if [ "$(docker ps -a | grep jms_koko)" ]; then
-            docker stop jms_koko
-            docker rm jms_koko
-        fi
-        docker run --name jms_koko -d -p $ssh_port:2222 -p 127.0.0.1:5000:5000 -e CORE_HOST=http://$Server_IP:8080 -e BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN --restart=always jumpserver/jms_koko:$Version
+    docker run --name jms_koko -d -p $ssh_port:2222 -p 127.0.0.1:5000:5000 -e CORE_HOST=http://$Server_IP:8080 -e BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN --restart=always jumpserver/jms_koko:$Version
+}
+
+function check_koko() {
+    if [ "$(docker exec jms_koko env | grep BOOTSTRAP_TOKEN | cut -d = -f2)" != "$BOOTSTRAP_TOKEN" ]; then
+        remove_koko
+        start_koko
+    fi
+    if [ ! "$(docker exec jms_koko env | grep $Server_IP )" ]; then
+        remove_koko
+        start_koko
     fi
 }
 
 function main() {
     if [ ! "$(docker ps | grep jms_koko:$Version)" ]; then
+        remove_koko
         start_koko
     else
-        if [ "$(docker exec jms_koko env | grep BOOTSTRAP_TOKEN | cut -d = -f2)" != "$BOOTSTRAP_TOKEN" ]; then
-            docker stop jms_koko
-            docker rm jms_koko
-            start_koko
-        fi
+        check_koko
     fi
 }
 
