fb0fbecc501e9f1e02e86b0ba0e7d89868b2c147
wojiushixiaobai <296015668@qq.com>
2020-04-21 09:43:17 +0800

    [Update]默认源替换为阿里云

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,12 +21,14 @@ function prepare_install() {
     if [ ! "$(rpm -qa | grep epel-release)" ]; then
         yum install -y epel-release
     fi
-    if grep -q 'mirrors.aliyun.com' /etc/yum.repos.d/epel.repo; then
-        true
-    else
+    if grep -q 'mirror.centos.org' /etc/yum.repos.d/CentOS-Base.repo; then
         wget -qO /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-        wget -qO /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
         sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
+        yum clean all
+    fi
+    if grep -q 'mirrors.fedoraproject.org' /etc/yum.repos.d/epel.repo; then
+        wget -qO /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+        sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/epel.repo
         yum clean all
     fi
     which git >/dev/null 2>&1
