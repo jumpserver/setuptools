c57af5e6f3910c3706a8f39cd520ddc75c193208
wojiushixiaobai <296015668@qq.com>
2020-08-09 13:00:38 +0800

    perf(repo): 更新 yum repo 源

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,12 +22,12 @@ function prepare_install() {
         yum install -y epel-release
     fi
     if grep -q 'mirror.centos.org' /etc/yum.repos.d/CentOS-Base.repo; then
-        wget -qO /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+        wget -qO /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
         sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
         yum clean all
     fi
     if grep -q 'mirrors.fedoraproject.org' /etc/yum.repos.d/epel.repo; then
-        wget -qO /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+        wget -qO /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo
         sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/epel.repo
         yum clean all
     fi
